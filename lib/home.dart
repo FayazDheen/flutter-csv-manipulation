635498ff7c26095df436b68accbc354450944519
FayazDheen <fayazdheen786@gmail.com>
2020-03-20 22:09:15 +0530

    20/03/2020

--- lib/home.dart
+++ lib/home.dart
@@ -1,8 +1,6 @@
 import 'package:flutter/material.dart';
-import 'dart:io';
 import 'dart:async';
-import 'dart:convert';
-// import 'package:flutter/services.dart' show rootBundle;
+import 'package:flutter/services.dart';
 
 class Home extends StatefulWidget {
   @override
@@ -10,41 +8,70 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
- 
-  csv() async {
-    final File file = new File("storage/emulated/0/csv/mem.csv"); //save csv locally in phone storage and give its path 
+  // csv() async {
+  //   final File file = new File(
+  //       "storage/emulated/0/csv/mem.csv"); //save csv locally in phone storage and give its path
+  //   print(file);
+  //   Stream<List> inputStream = file.openRead();
+  //   inputStream
+  //       .transform(utf8.decoder) // Decode bytes to UTF-8.
+  //       .transform(new LineSplitter()) // Convert stream to individual lines.
+  //       .listen((String line) {
+  //     // Process results.
+  //     List row = line.split(','); // split by comma
 
-    Stream<List> inputStream = file.openRead();
-    inputStream
-        .transform(utf8.decoder) // Decode bytes to UTF-8.
-        .transform(new LineSplitter()) // Convert stream to individual lines.
-        .listen((String line) {
-      // Process results.
-      List row = line.split(','); // split by comma
+  //     String sno = row[0];
+  //     String name = row[1];
+  //     String designation = row[2];
+  //     if (designation == "Staff") {
+  //       print('$sno, $name, $designation');
+  //     }
+  //   }, onDone: () {
+  //     print('File is now closed.');
+  //   }, onError: (e) {
+  //     print(e.toString());
+  //   });
+  // }
 
-      String sno = row[0];
-      String name = row[1];
-      String designation = row[2];
-      if (designation == "Staff") {
-        print('$sno, $name, $designation');
+  Future<String> loadAsset(String path) async {
+    return await rootBundle.loadString(path);
+  }
+  
+  void loadCSV() {
+    loadAsset('asset/mem.csv').then((dynamic output) {
+      print(output);
+      String test = output;
+      var row = test.split('\r\n');
+      for (var item in row) {
+        var values = item.split(',');
+        for (var i in values) {
+          print(i);
+        }
       }
-    }, onDone: () {
-      print('File is now closed.');
-    }, onError: (e) {
-      print(e.toString());
     });
   }
 
   @override
   void initState() {
-    csv();
+    // csv();
+    loadCSV();
     super.initState();
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text("HOME")),
+      appBar: AppBar(
+        title: Text("HOME"),
+        actions: <Widget>[
+          IconButton(
+            icon: Icon(Icons.refresh),
+            onPressed: () {
+              loadCSV();
+            },
+          )
+        ],
+      ),
       body: Container(),
     );
   }
--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,7 @@
 import 'package:flutter/material.dart';
-
-
 import 'home.dart';
 
 void main() => runApp(MyApp());
-
 class MyApp extends StatefulWidget { 
   @override
   _MyAppState createState() => _MyAppState();
@@ -21,86 +18,3 @@ class _MyAppState extends State<MyApp> {
     );
   }
 }
-// class MyHomePage extends StatefulWidget {
-//   MyHomePage({Key key, this.title}) : super(key: key);
-//   final String title;
-
-//   @override
-//   _MyHomePageState createState() => _MyHomePageState();
-// }
-
-// class _MyHomePageState extends State<MyHomePage> {
-
-
-
-
-//   int _counter = 0;
-
-//   void _incrementCounter() {
-//     setState(() {
-      
-//       _counter++;
-//     });
-//   }
-  
-//   @override
-//   Widget build(BuildContext context) {
-    
-//     return Scaffold(
-//       appBar: AppBar(
-//         title: Text(widget.title),
-//       ),
-//       body: Center(
-//         child: Column(
-//           mainAxisAlignment: MainAxisAlignment.center,
-//           children: <Widget>[
-//             Text(
-//               'You have pushed the button this many times:',
-//             ),
-//             Text(
-//               '$_counter',
-//               style: Theme.of(context).textTheme.display1,
-//             ),
-//           ],
-//         ),
-//       ),
-//       floatingActionButton: FloatingActionButton(
-//         onPressed: _incrementCounter,
-//         tooltip: 'Increment',
-//         child: Icon(Icons.add),
-//       ), 
-//     );
-//   }
-// }
-
-
-
-
-
-// main(List arguments) async {
-// //   Future<String> getFileData(String path) async {
-// //   return await rootBundle.loadString(path);
-// // }
-// //   String data = await getFileData("assets/mem.csv");
-// //   print(data);
-//   final File file = new File("storage/emulated/0/csv/mem.csv");
-//   // String data = await  rootBundle.loadString("assets/mem.csv");
-//   // print(data);
-//   Stream<List> inputStream = file.openRead();
-//   inputStream
-//       .transform(utf8.decoder)       // Decode bytes to UTF-8.
-//       .transform(new LineSplitter()) // Convert stream to individual lines.
-//       .listen((String line) {   
-//         // print(line.length);     // Process results.
-//        List row = line.split(','); 
-//                                  // split by comma
-//         String sno = row[0];
-//         String name = row[1];
-//         String designation = row[2];
-//         if(designation=="Staff"){
-//           print('$sno, $name, $designation');
-//         }
-//       },
-//       onDone: () { print('File is now closed.'); },
-//       onError: (e) { print(e.toString()); });
-// }
